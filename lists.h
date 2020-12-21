0fbc785f68f208459436df379252cb3063050d8b
William Colindres <1689@holbertonschool.com>
2020-12-21 23:49:41 +0000

    added nop function

--- lists.h
+++ lists.h
@@ -46,6 +46,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 void *add_dnode_int_end(stack_t **head, int n);
 int main(int argc, char *argv[]);
 
