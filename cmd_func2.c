d9c5b5ab029d623ef13fc24240a41b44f1c97269
William Colindres <1689@holbertonschool.com>
2020-12-21 23:51:40 +0000

    added the nop function

--- cmd_func2.c
+++ cmd_func2.c
@@ -6,3 +6,9 @@ void add(stack_t **stack, unsigned int line_number)
     line_number++;
     printf("add");
 }
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
